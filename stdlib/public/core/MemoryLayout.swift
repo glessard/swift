9eb6905767ef5a27187de9864a948801cba399f3
Guillaume Lessard <guillaume.lessard@apple.com>
2024-02-23 22:35:36 -0800

    [stdlib] MemoryLayout: support non-escapable type arguments
    # Conflicts:
    #	stdlib/public/core/MemoryLayout.swift

--- stdlib/public/core/MemoryLayout.swift
+++ stdlib/public/core/MemoryLayout.swift
@@ -40,12 +40,12 @@
 ///             byteCount: count * MemoryLayout<Point>.stride,
 ///             alignment: MemoryLayout<Point>.alignment)
 @frozen // namespace
-public enum MemoryLayout<T: ~Copyable> {}
+public enum MemoryLayout<T: ~Copyable & ~Escapable> {}
 
 @available(*, unavailable)
 extension MemoryLayout : _BitwiseCopyable {}
 
-extension MemoryLayout where T: ~Copyable {
+extension MemoryLayout where T: ~Copyable & ~Escapable {
   /// The contiguous memory footprint of `T`, in bytes.
   ///
   /// A type's size does not include any dynamically allocated or out of line
@@ -84,7 +84,7 @@ extension MemoryLayout where T: ~Copyable {
   }
 }
 
-extension MemoryLayout where T: ~Copyable {
+extension MemoryLayout where T: ~Copyable & ~Escapable {
   /// Returns the contiguous memory footprint of the given instance.
   ///
   /// The result does not include any dynamically allocated or out of line
@@ -247,7 +247,7 @@ extension MemoryLayout {
 }
 
 // Not-yet-public alignment conveniences
-extension MemoryLayout where T: ~Copyable {
+extension MemoryLayout where T: ~Copyable & ~Escapable {
   internal static var _alignmentMask: Int { return alignment - 1 }
 
   internal static func _roundingUpToAlignment(_ value: Int) -> Int {

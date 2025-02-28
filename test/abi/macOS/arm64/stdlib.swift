8814f9d67d6d14615b7eac309f7b12f9160ece43
Guillaume Lessard <guillaume.lessard@apple.com>
2025-02-27 22:31:45 -0800

    [test] abi accounting

--- test/abi/macOS/arm64/stdlib.swift
+++ test/abi/macOS/arm64/stdlib.swift
@@ -976,6 +976,16 @@ Added: _$ss15ContiguousArrayV11mutableSpans07MutableD0VyxGvr
 Added: _$ss11InlineArrayVsRi__rlE11mutableSpans07MutableD0Vyq_Gvr
 Added: _$ss15CollectionOfOneV11mutableSpans07MutableE0VyxGvr
 
+// OutputSpan
+Added: _$ss10OutputSpanVMa
+Added: _$ss10OutputSpanVMn
+Added: _$ss10OutputSpanVsRi_zrlE12_initializedSivM
+Added: _$ss10OutputSpanVsRi_zrlE12_initializedSivg
+Added: _$ss10OutputSpanVsRi_zrlE12_initializedSivs
+Added: _$ss10OutputSpanVsRi_zrlE8_pointerSvSgvg
+Added: _$ss10OutputSpanVsRi_zrlE8capacitySivg
+Added: _$ss10OutputSpanVsRi_zrlEfD
+
 // _SwiftifyInfo enum for _SwiftifyImports macro
 Added: _$ss13_SwiftifyExprO5paramyABSicABmFWC
 Added: _$ss13_SwiftifyExprO6returnyA2BmFWC
